82faf008d786578008187a85f0cd6df457cf42a4
Vivek <vtumrukota@gmail.com>
2015-07-21 20:30:17 -0700

    Add my Picture to Profile

--- snapmarketapp/app/scripts/services.js
+++ snapmarketapp/app/scripts/services.js
@@ -11,7 +11,7 @@ angular.module('starter.services', [
  })
  .run(function($rootScope, $state, $ionicTabsDelegate, Db, Camera, $firebaseObject){
   //for demo to circumvent auth, change $rootScope.production to false
-  $rootScope.production = true;
+  $rootScope.production = false;
   $rootScope.TESTUSER = JSON.parse('{"uid":"facebook:10204542119488571","provider":"facebook","facebook":{"id":"10204542119488571","accessToken":"CAAHsTNfTaFsBAPxxsY9SVQrIY4pbgppVjZAmGmO22nkyYRvDZAzQXIOTjd0ODsaUcKZBC5jTLZCk8gRHZCVXdsFqMHKeOMtXcpLlqXoURydNQ1CpZBxZA4eRzonCIwriTsNeRIWzjZBbsxFvXWrbscjRgL3XVZA37RyF5m2nbBuisUVsDiUgro1ChFNXaZCriufZCGraiIzXJrqR6LZBtx9KqYg0","displayName":"Vivek Tumrukota","email":"vtumrukota@gmail.com","cachedUserProfile":{"id":"10204542119488571","name":"Vivek Tumrukota","last_name":"Tumrukota","first_name":"Vivek","gender":"male","link":"https://www.facebook.com/app_scoped_user_id/10204542119488571/","email":"vtumrukota@gmail.com","picture":{"data":{"is_silhouette":false,"url":"https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xap1/v/t1.0-1/p100x100/109…2cf1841106&oe=55F431B2&__gda__=1441461351_782986e2ae99c1d2d30b38d21dac6229"}},"age_range":{"min":21},"locale":"en_US","timezone":-7}},"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InVpZCI6ImZhY2Vib29rOjEwMjA0NTQyMTE5NDg4NTcxIiwicHJvdmlkZXIiOiJmYWNlYm9vayJ9LCJpYXQiOjE0MzQwNjAwNDJ9.n7GLG9y_EHnzPj0ubVngfSOVELG8MVo-uT_4Nw8tpdc","auth":{"uid":"facebook:10204542119488571","provider":"facebook"},"expires":1434146442}');
   
   //check for authentication prior to state change
